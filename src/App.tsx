6ac2aa768b6d1f1e950a446f7e408f08cc4564aa
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2022-07-29 09:23:13 -0300

    fix routing for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Connect from './pages/connect/Connect';
 import SelectProfile from './pages/select-profile/SelectProfile';
 import Claimer from './pages/claimer/Claimer';
@@ -16,7 +15,7 @@ import ClaimDetail from './pages/claimer/detail/ClaimDetail';
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Connect />}></Route>
         <Route path="/select-profile" element={<SelectProfile />}> </Route>
@@ -30,7 +29,7 @@ function App() {
         <Route path="/attester/requests" element={<AttesterRequests />}> </Route>
         <Route path="/attester/requests/:id" element={<AttesterRequestDetail />}> </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
     </div>
   );
 }
