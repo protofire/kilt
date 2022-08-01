9b285bd0852dbb952c20697b39da43118246ae19
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2022-08-01 13:42:37 -0300

    fix table warnings

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -24,24 +24,32 @@ interface Props {
 function Table({ columns, rows, onClick = () => {}, disabled = false } : Props) {
   return (
     <table>
-    <tr>
-      {columns.map(c => 
-        <th key={c.name}>{c.name}</th> 
-      )}
-    </tr>
-    {rows.map(r => 
-      <Fragment key={r.id}>
-        <div style={{width: (rows.length * 100)+'%'}}>
-          <hr />
-        </div>
-        <tr className={disabled ? '' : 'clickable'} 
-          onClick={() => !disabled && onClick(r.id)}>
-            {r.values.map(val => 
-              <td style={val.color ? {color: val.color}:{}}>{val.value}</td>
-            )}
-        </tr> 
-      </Fragment>
-    )}
+      <thead>
+        <tr>
+        {columns.map(c => 
+          <th key={c.name}>{c.name}</th> 
+        )}
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map(r => 
+          <Fragment key={r.id}>
+            <div style={{width: (columns.length * 100)+'%'}}>
+              <hr />
+            </div>
+            <tr className={disabled ? '' : 'clickable'} 
+              onClick={() => !disabled && onClick(r.id)}>
+                {r.values.map(val => 
+                  <td 
+                    key={r.id + '' + val.value}
+                    style={val.color ? {color: val.color}:{}}>
+                      {val.value}
+                  </td>
+                )}
+            </tr> 
+          </Fragment>
+        )}
+      </tbody>
     </table>
   );
 }
