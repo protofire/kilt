66ad196a16fbbf28ed03b60e861fb8eec7ba9b58
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2022-08-01 14:31:44 -0300

    added disconnect button

--- src/pages/connect/Connect.tsx
+++ src/pages/connect/Connect.tsx
@@ -1,26 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useUser from '../../hooks/user';
 
 function Connect() {
   const navigate = useNavigate();
-  const { login, user } = useUser();
-  
+  const { login, logout, user } = useUser();
+
   const [did, setDid] = useState<string>('');
   
   const connect = async () => {
     await login(did);
-    if(!user) return;
+    if (!user) return;
     if (user.isAttester) navigate('/select-profile');
     else navigate('/claimer');
   }
+
+  useEffect(() => {
+    setDid(user?.did ?? '');
+  }, [ user ]);
 
   return (
     <div className='wrapper'>
       <div className='center column'>
         <input type="text" value={did} placeholder={'did'} onChange={(e) => setDid(e.target.value)} />
-        <button className='connect-btn' onClick={connect}>
+        <button className='primary' onClick={connect}>
           Connect
+        </button>
+        <button className='secondary' onClick={logout}>
+          Disconnect
         </button>
       </div>
     </div>
