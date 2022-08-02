5b08dd09aa7b35d7b4dc7ad9b644c2c2e549fa88
Mike Udoh <miceuud@gmail.com>
2022-08-02 13:28:11 +0100

    WIP: Broken endpoint fixed

--- server/controller/attester.ts
+++ server/controller/attester.ts
@@ -1,35 +1,6 @@
 import { Request, Response } from "express";
 
-const attesters = [
-  "did:kilt:4pyAEJGKaS6mJhFcsM7XXTnSM7JVaAf1z4FhADicquou9LWC",
-  "did:kilt:4pyAEJGKaS6mJhFcsM7XXTnSM7JVaAf1z4FhADicquou9LWre",
-  "did:kilt:4pyAEJGKaS6mJhFcsM7XXTnSM7JVaAf1z4FhADicq9LWC",
-];
-
-// @desc      Create new attesters
-// @route     POST /api/attesters
-// @access    Private
-
-export async function createAttester(req: Request, res: Response) {
-  const { did } = req.body;
-
-  if (!did) {
-    return res.status(404).json({
-      status: false,
-      data: {
-        did: "Please provide a valid did",
-      },
-    });
-  }
-  attesters.push(did);
-  return res.status(200).json({
-    status: true,
-    data: {
-      did,
-      message: "DID successfully added to database",
-    },
-  });
-}
+const attesters = ["did:kilt:4pyAEJGKaS6mJhFcsM7XXTnSM7JVaAf1z4FhADicquou9LWC"];
 
 // @desc      Get all attesters
 // @route     GET /api/attesters
--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,17 @@
+
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-
+import cors from "cors";
+import bodyParser from "body-parser";
+import { router as attesters} from './routes/attester';
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT;
 
@@ -19,3 +25,4 @@ app.use("/api/attesters", attesters);
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
+ 
