99a9764d741b8d6ad19f274cd71c884bfa205719
Mike Udoh <miceuud@gmail.com>
2022-08-02 12:55:44 +0100

    WIP: Attester endpoint

--- server/index.ts
+++ server/index.ts
@@ -1,22 +1,25 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import { userRouter } from "./routes/user";
-import { logoutRouter } from "./routes/logout";
-import { secretRouter } from "./routes/secret";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+import { router as attesters } from "./routes/attester";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const port = process.env.PORT;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Test");
+  res.send("Kilt cookbook recipe");
 });
 
 // routes
-app.use("/logout", logoutRouter);
-app.get("/secret", secretRouter);
-app.use("/user", userRouter);
+app.use("/api/attesters", attesters);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
