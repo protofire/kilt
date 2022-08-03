fd289858be10dd53059db5c4b342867482bdc085
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2022-08-03 17:41:47 -0300

    prevent wrong formatted DiD

--- server/controller/claimController.ts
+++ server/controller/claimController.ts
@@ -6,15 +6,28 @@ import { buildCredential, getEndpointResponse, getEndpointsFromDid } from "../ut
 export async function getCredentialsByDid(req: Request, res: Response) {
   const { did } = req.params;
 
+  if (!did) {
+    return res.status(400).json({ 
+      success: false, 
+      msg: 'Must provide DiD parameter' 
+    });
+  }
+
+  if (!did.startsWith('did:kilt:')) {
+    return res.status(400).json({ 
+      success: false, 
+      msg: 'Wrong DiD format' 
+    });
+  }
+
   const endpoints = await getEndpointsFromDid(did as DidUri);
   if (!endpoints) {
     return res.status(200).json({ success: true, data: [] });
   }
 
   const endpointResponse = await getEndpointResponse(endpoints[0]);
-
-  const credentials: ICredentialByDidResponse[] = await Promise.all(
-    endpointResponse.map(buildCredential));
+  const credentials: ICredentialByDidResponse[] = await Promise
+    .all(endpointResponse.map(buildCredential));
 
   return res.status(200).json({success: true, data: credentials});
-  }
+}
--- server/utils/claimUtils.ts
+++ server/utils/claimUtils.ts
@@ -1,8 +1,15 @@
 import axios from 'axios';
-import { ICredentialEndpointResponse } from "../interfaces/credentialEndpointResponse";
+import { ICredentialByDidResponse, ICredentialEndpointResponse } from "../interfaces/credentialEndpointResponse";
 import { Attestation, Did, DidServiceEndpoint, DidUri, Credential } from "@kiltprotocol/sdk-js";
 
-// set of utilities for handling sdk operations for claim.
+/**
+ *  set of utilities for handling sdk operations for claims.
+ */
+
+class Status {
+  static verified = 'verified';
+  static unverified = 'unverified';
+}
 
 const getEndpointsFromDid = async (did: DidUri) => {
   const didDocument = await Did.DidResolver.resolveDoc(did as DidUri);
@@ -20,9 +27,9 @@ const buildCredential = async (endpointData: ICredentialEndpointResponse) => {
   const attestation = await Attestation.query(endpointData.credential.rootHash);
   if (!attestation) {
     return {
-      attesterDid: 'No Attester',
+      attesterDid: '',
       label: endpointData.metadata?.label,
-      status: 'unverified'
+      status: Status.unverified
     }
   }
   const credential = Credential.fromRequestAndAttestation(endpointData.credential, attestation);
@@ -30,7 +37,7 @@ const buildCredential = async (endpointData: ICredentialEndpointResponse) => {
   return {
     attesterDid: attestation.owner,
     label: endpointData.metadata?.label,
-    status: isVerified ? 'verified' : 'unverified'
+    status: isVerified ? Status.verified : Status.unverified
   }
 }
 
