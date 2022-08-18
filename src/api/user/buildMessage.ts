5fd664fda0410d11a47c1768858de785f41dddbd
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2022-08-18 18:59:47 -0300

    code cleanup

--- src/api/user/buildMessage.ts
+++ src/api/user/buildMessage.ts
@@ -9,11 +9,7 @@ export const buildMessage = async (encryptionKeyId: string) => {
   const { baseUrl, headers } = apiConfig();
   const response = await fetch(`${baseUrl}/api/user/message`, {
     method: 'POST',
-    body: JSON.stringify({
-      encryptionKeyId
-      // challenge,
-      // nonce
-    }),
+    body: JSON.stringify({ encryptionKeyId }),
     headers
   });
   const { data } = await response.json();
