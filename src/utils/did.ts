eab847e8edfea5f454587f7d7d3f6ce3a79d5294
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2022-08-19 16:10:13 -0300

    added last character in did address when no web3name present

--- src/utils/did.ts
+++ src/utils/did.ts
@@ -1,10 +1,12 @@
 import { DidUri } from '@kiltprotocol/sdk-js';
 import { IUser } from '../interfaces/user';
 
-export const formatDidUri = (did: DidUri) =>
-  did.substring(0, 12) +
-  '...' +
-  did.substring(did.length - 5, did.length - 1);
+export const formatDidUri = (didUri: DidUri) => {
+  const did = didUri.split('#')[0];
+  const formattedDid = did.substring(0, 12) + '...' +
+    did.substring(did.length - 5, did.length);
+  return formattedDid;
+};
 
 export const getDisplayName = (user: IUser) => {
   return user.web3name ?? formatDidUri(user.didUri);
